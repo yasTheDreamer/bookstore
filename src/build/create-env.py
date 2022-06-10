6fe64c91ac34d34b58fd7ea56bb146a603e7d14a
yasTheDreamer <yassine19265@gmail.com>
2022-06-10 21:53:48 +0100

    test: check pip list

--- src/build/create-env.py
+++ src/build/create-env.py
@@ -11,24 +11,19 @@ def main(args=None):
     parser.add_argument('--python', required=True)
 
     env_variables = os.environ.copy()
-    miniconda_env_name = "C:\\ProgramData\\Anaconda3"
-    conda_path = os.path.join(miniconda_env_name, 'Scripts', 'conda.exe')
+    miniconda_env_name = r"C:\ProgramData\Anaconda3"
 
     params = vars(parser.parse_args(args))
     virtualenv = params['venv']
     requirements_file = params['requirements']
     python_version = params['python']
 
-    pip_path = os.path.join(virtualenv, 'Scripts', 'pip.exe')
+    conda_path = os.path.join(miniconda_env_name, 'Scripts', 'conda.exe')
+    pip_path = os.path.join(miniconda_env_name, 'Scripts', 'pip.exe')
+    activate_path = os.path.join(miniconda_env_name, 'Scripts', 'activate')
 
-    print("Cleaning conda ...")
-    errcode = subprocess.call([conda_path, 'clean', '-yl'], shell=True)
-
-    if errcode:
-        return errcode
-
-    print('Creating virtual env ....')
-    conda_create = [conda_path, 'create', '-y', '-p', virtualenv]
+    print(f'Creating virtual env {pip_path}....')
+    conda_create = [conda_path, 'create', '-y', '-p', virtualenv, f'python={python_version}']
     error_code = subprocess.call(conda_create, shell=True)
     if error_code:
         print("Creating venv failed")
@@ -37,11 +32,22 @@ def main(args=None):
     print("virtual environement created successfully")
     print("virtual env is stored in %s" % virtualenv)
 
+    print('activating venv ...')
+    error_code = subprocess.call(['conda', 'activate', virtualenv], shell=True)
+    if error_code:
+        print("could not activate venv")
+        return error_code
+
+    print("venv activated successfully")
+
+    print("installing requirements ....")
+
     requirements_install = [pip_path, 'install', '-r', requirements_file]
     error_code = subprocess.call(requirements_install, shell=True)
     if error_code:
         print("Installing requirements failed")
         return error_code
+
     print("requirements installed successfully")
 
     return 0
--- src/tests/unit/simple_mock_test.py
+++ src/tests/unit/simple_mock_test.py
@@ -1,8 +1,10 @@
 import pytest
+from pytest_mock import MockerFixture
+
 from src.services.users.sum import using_sum
 
 
-def test_simple_sum(mocker):
+def test_simple_sum(mocker: MockerFixture):
     mocker.patch('src.services.users.sum.sum_example', return_value=2)
 
     assert using_sum() == 2
